204db6fd24cccc9d7f8fad148a52eb0471e85604
Osvaldo Rodriguez <lavso0393@gmail.com>
2024-02-20 10:44:50 -0500

    creating extension lists for file classification

--- organize_file.py
+++ organize_file.py
@@ -1,3 +1,21 @@
 import os
+from os.path import isfile ,join
 import shutil
+
+
+file_path = os.getcwd()
+
+DOCUMENTS = ('txt', 'doc', 'docx', 'pdf', 'odt', 'rtf', 'ppt', 'pptx', 'xls', 'xlsx', 'csv')
+VIDEO = ('mp4', 'avi', 'mkv', 'mov', 'wmv', 'flv', 'webm', 'm4v', '3gp', 'mpeg', 'mpg')
+AUDIO = ('mp3', 'wav', 'flac', 'aac', 'ogg', 'wma', 'm4a', 'aiff', 'opus', 'alac', 'pcm')
+PICTURES = ('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'svg')
+EXCECUTABLES = ('exe', 'dll', 'bat', 'com', 'sh', 'app', 'msi', 'jar', 'run', 'deb')
+
+files = [f for f in os.listdir(file_path) if isfile(join(file_path, f))]
+
+def organizer(file_type):
+    for file in files:
+        archive = file.split('.')
+        if archive[1] in file_type:
+            print(file)
 
